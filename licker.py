ea81767b495f0efce5452f96ea38ad5addc97ce4
Jan Brennenstuhl <jan.brennenstuhl@immobilienscout24.de>
2015-03-20 13:50:34 +0100

    now with docopt and proper statements.

--- licker.py
+++ licker.py
@@ -14,51 +14,92 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+"""
+usage:
+    licker.py [TOKEN] [options]
+
+options:
+    -o, --organisation=ORGANISATION  the Github organisation you want to check [default: ImmobilienScout24]
+"""
+
 import urllib.request
 import json
 
-page_parameter = '&page='
-api = 'https://api.github.com'
-organisation = 'ImmobilienScout24'
-repository_url = '%s/orgs/%s/repos?type=all' % (api, organisation)
-license_url = '%s/repos/%s/' % (api, organisation)
-token = ''
-
-with open('GITHUB_TOKEN', 'r') as fd:
-    token = fd.readline().strip()  # Can't hurt to be paranoid
-
-request_headers = {
-    'Authorization': 'token %s' % token,
-    'Accept': 'application/vnd.github.drax-preview+json'}
-
-json_response = repositories = []
-page_number = 1
-
-while json_response or page_number == 1:
-    request = urllib.request.Request(
-        url='%s%s%s' % (repository_url, page_parameter, page_number),
-        headers=request_headers)
-
-    response = urllib.request.urlopen(request).read()
-    json_response = json.loads(response.decode('utf-8'))
-    repositories.extend(json_response)
-
-    page_number += 1
+from docopt import docopt
 
 
-for repository in repositories:
-    if not repository['fork']:
+def run():
+    arguments = docopt(__doc__)
+    organisation = arguments["--organisation"]
+    token = arguments['TOKEN']
+
+    if not token:
+        with open('GITHUB_TOKEN', 'r') as fd:
+            token = fd.readline().strip()  # Can't hurt to be paranoid
+
+    api = 'https://api.github.com'
+    repository_url = '%s/orgs/%s/repos?type=all' % (api, organisation)
+    license_url = '%s/repos/%s/' % (api, organisation)
+    page_parameter = '&page='
+
+    request_headers = {
+        'Authorization': 'token %s' % token,
+        'Accept': 'application/vnd.github.drax-preview+json'}
+
+    json_response = repositories = []
+    page_number = 1
+
+    print('start connecting to github...')
+    while json_response or page_number == 1:
         request = urllib.request.Request(
-            url=license_url + repository['name'],
+            url='%s%s%s' % (repository_url, page_parameter, page_number),
             headers=request_headers)
+
+        if page_number == 1:
+            print('start collecting repositories...')
 
         response = urllib.request.urlopen(request).read()
         json_response = json.loads(response.decode('utf-8'))
+        repositories.extend(json_response)
 
-        if json_response['license']:
-            print('%s -- %s' % (repository['name'], json_response['license']['name']))
-        else:
-            print('%s -- %s' % (repository['name'], 'NO LICENSE'))
+        page_number += 1
+
+    repos_with_license = []
+    repos_without_license = []
+
+    print('start fetching license information...')
+    for repository in repositories:
+        if not repository['fork']:
+            request = urllib.request.Request(
+                url=license_url + repository['name'],
+                headers=request_headers)
+
+            response = urllib.request.urlopen(request).read()
+            json_response = json.loads(response.decode('utf-8'))
+
+            if json_response['license']:
+                repos_with_license.append({
+                    'name': repository['name'],
+                    'license': json_response['license']['name']})
+            else:
+                repos_without_license.append(repository['name'])
+                # print('\t%s -- %s' % (repository['name'], 'NO LICENSE'))
+
+    if repos_with_license:
+        print('\nfound %s repositories with license:\n' % len(repos_with_license))
+        for repository in repos_with_license:
+            print('\t%s -- %s' % (repository['name'], repository['license']))
+
+    if repos_without_license:
+        print('\nfound %s repositories without license:\n' % len(repos_without_license))
+        for repository in repos_without_license:
+            print('\t%s' % repository)
+
+
+if __name__ == "__main__":
+    run()
+
 
 
 
